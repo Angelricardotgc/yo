72206eb7307e9a1dd571ac84e4610161b7171088
Angelricardotgc <atellezgironcastro@gmail.com>
2024-12-02 18:29:09 -0600

    Update laboratorio2.c

--- laboratorio2.c
+++ laboratorio2.c
@@ -2,7 +2,7 @@
 
 // Función que calcula la máxima cantidad de agua que se puede contener
 int maxArea(int* height, int n) {
-    int max_area = 0;         // Variable para almacenar el área máxima
+    int max_area = 0;         
     int left = 0;              // Puntero al inicio del arreglo
     int right = n - 1;         // Puntero al final del arreglo
     
@@ -26,23 +26,23 @@ int maxArea(int* height, int n) {
         }
     }
     
-    return max_area;
+    return max_area;  // Devuelve el área máxima encontrada
 }
 
 int main() {
     // Ejemplo 1
-    int height1[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-    int n1 = sizeof(height1) / sizeof(height1[0]);
-    int result1 = maxArea(height1, n1);
-    printf("Ejemplo 1:\nInput: height = [1,8,6,2,5,4,8,3,7]\n");
-    printf("Salida: Maxima cantidad de agua que se puede contener: %d\n\n", result1);
+    int height1[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};  
+    int n1 = sizeof(height1) / sizeof(height1[0]);  // Calcula el número de elementos en height1
+    int result1 = maxArea(height1, n1);  // Llama a la función maxArea para calcular el área máxima
+    printf("Ejemplo 1:\nInput: height = [1,8,6,2,5,4,8,3,7]\n"); 
+    printf("Salida: Maxima cantidad de agua que se puede contener: %d\n\n", result1);  
     
     // Ejemplo 2
-    int height2[] = {1, 1};
-    int n2 = sizeof(height2) / sizeof(height2[0]);
-    int result2 = maxArea(height2, n2);
-    printf("Ejemplo 2:\nInput: height = [1,1]\n");
-    printf("Salida: Maxima cantidad de agua que se puede contener: %d\n", result2);
+    int height2[] = {1, 1};  
+    int n2 = sizeof(height2) / sizeof(height2[0]);  // Calcula el número de elementos en height2
+    int result2 = maxArea(height2, n2);  // Llama a la función maxArea para calcular el área máxima
+    printf("Ejemplo 2:\nInput: height = [1,1]\n");  
+    printf("Salida: Maxima cantidad de agua que se puede contener: %d\n", result2);  
     
-    return 0;
+    return 0; 
 }
